b60b0efb2d8d9a91c8362a8c8e0be52bce48f7f5
d4nzS <dzianis.karym@gmail.com>
2023-09-21 14:31:52 +0300

    Create login screen with naviagtion

---
+++ client/src/components/Auth/Login/Login.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import AuthLayout from '../AuthLayout';
+
+const Login: FC = () => {
+  return (
+    <AuthLayout title="Login"></AuthLayout>
+  );
+};
+
+export default Login;
---
+++ client/src/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import Login from '../components/Auth/Login/Login';
+
+const LoginScreen: FC = () => {
+  return <Login/>;
+};
+
+export default LoginScreen;
