3c3df40ae285cba7bd666baec8fd7e35cc953d8f
d4nzS <dzianis.karym@gmail.com>
2023-09-22 12:38:27 +0300

    Configure environment and db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNMA}:${process.env.MONGO_PASSWORD}@cluster0.ct19jhx.mongodb.net/`
+    )
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
